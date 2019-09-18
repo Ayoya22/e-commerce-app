a94c60c212717df085570caf6cb1a49d188dcd84
Ayoya22 <djoukamsimplice@gmail.com>
2019-09-18 14:48:17 +0100

    Updated the code
    Signed-off-by: Ayoya22 <djoukamsimplice@gmail.com>

--- src/main/java/com/example/demo/model/persistence/Cart.java
+++ src/main/java/com/example/demo/model/persistence/Cart.java
@@ -15,113 +15,70 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-/**
- * The Class Cart.
- */
+
 @Entity
 @Table(name = "cart")
 public class Cart {
 	
-	/** The id. */
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@JsonProperty
 	@Column
 	private Long id;
 	
-	/** The items. */
+
 	@ManyToMany
 	@JsonProperty
 	@Column
     private List<Item> items;
 	
-	/** The user. */
+
 	@OneToOne(mappedBy = "cart")
 	@JsonProperty
     private ApplicationUser user;
 	
-	/** The total. */
+
 	@Column
 	@JsonProperty
 	private BigDecimal total;
 	
-	/**
-	 * Gets the total.
-	 *
-	 * @return the total
-	 */
+
 	public BigDecimal getTotal() {
 		return total;
 	}
 
-	/**
-	 * Sets the total.
-	 *
-	 * @param total the new total
-	 */
+
 	public void setTotal(BigDecimal total) {
 		this.total = total;
 	}
 
-	/**
-	 * Gets the user.
-	 *
-	 * @return the user
-	 */
+
 	public ApplicationUser getUser() {
 		return user;
 	}
 
-	/**
-	 * Sets the user.
-	 *
-	 * @param user the new user
-	 */
+
 	public void setUser(ApplicationUser user) {
 		this.user = user;
 	}
 
-	/**
-	 * Gets the id.
-	 *
-	 * @return the id
-	 */
 	public Long getId() {
 		return id;
 	}
 
-	/**
-	 * Sets the id.
-	 *
-	 * @param id the new id
-	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-	/**
-	 * Gets the items.
-	 *
-	 * @return the items
-	 */
 	public List<Item> getItems() {
 		return items;
 	}
 
-	/**
-	 * Sets the items.
-	 *
-	 * @param items the new items
-	 */
 	public void setItems(List<Item> items) {
 		this.items = items;
 	}
-	
-	/**
-	 * Adds the item.
-	 *
-	 * @param item the item
-	 */
+
 	public void addItem(Item item) {
 		if(items == null) {
 			items = new ArrayList<>();
@@ -132,12 +89,7 @@ public class Cart {
 		}
 		total = total.add(item.getPrice());
 	}
-	
-	/**
-	 * Removes the item.
-	 *
-	 * @param item the item
-	 */
+
 	public void removeItem(Item item) {
 		if(items == null) {
 			items = new ArrayList<>();
